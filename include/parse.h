0b3279bb1df6689aef585596c38fc7b18be7a36b
Kamron Smith <kamrosmith@gmail.com>
2026-01-07 15:40:58 -0800

    Reindented parse.h

--- include/parse.h
+++ include/parse.h
@@ -4,16 +4,16 @@
 #define HEADER_MAGIC 0x4c4c4144
 
 struct dbheader_t {
-	unsigned int magic;
-	unsigned short version;
-	unsigned short count;
-	unsigned int filesize;
+    unsigned int magic;		// Allows the program to know if the database is correct
+    unsigned short version;	// The version number of the database
+    unsigned short count;	// Amount of employees tracked in the database
+    unsigned int filesize;	// Filesize of the database
 };
 
 struct employee_t {
-	char name[256];
-	char address[256];
-	unsigned int hours;
+    char name[256];
+    char address[256];
+    unsigned int hours;
 };
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
